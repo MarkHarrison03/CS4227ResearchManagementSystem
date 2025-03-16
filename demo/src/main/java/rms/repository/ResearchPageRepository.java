5511dca50a1f180d342c09088d9709bddad4e825
Mark <21335265@studentmail.ul.ie>
2025-03-16 22:34:06 +0000

    Updates

--- demo/src/main/java/rms/repository/ResearchPageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.rms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.rms.model.ResearchPage;
-
-@Repository
-public interface ResearchPageRepository extends JpaRepository<ResearchPage, Long> {
-}
